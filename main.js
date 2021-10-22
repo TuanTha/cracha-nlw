536f9b29a3b6a54d53cdb3b41e8d0ff287cf7732
ThuanTha <tauan.silva00@gmail.com>
2021-10-22 15:43:34 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'TuanTha',
+  youtube: 'UCYZ-0vSnJcohqQhVevbGNLQ',
+  facebook: 'Tauan Silva',
+  instagram: 'tauansilva00',
+  twitter: 'Bel0_ved'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
